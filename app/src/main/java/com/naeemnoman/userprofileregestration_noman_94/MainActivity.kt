9cfbfcc02165a3634f78e693b1e9fc6f1414d6c7
naeemnoman <naeemnoman0419@gmail.com>
2024-11-09 17:04:31 +0600

    done

---
+++ app/src/main/java/com/naeemnoman/userprofileregestration_noman_94/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.naeemnoman.userprofileregestration_noman_94
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var listButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+     listButton = findViewById(R.id.profileListBtn)
+
+        listButton.setOnClickListener {
+            startActivity(Intent(this, ProfileListActivity::class.java))
+            finish()
+        }
+    }
+}
